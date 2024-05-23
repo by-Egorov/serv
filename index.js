9c5f113759d121798b768e114ec6a3737ef17a88
by-Egorov <egorov,dev@gmail.com>
2024-05-23 06:16:14 +0300

    start server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const server = express()
+const PORT = 5000
+
+server.get('/', (req, res) => {
+	res.send("auvtor - Egorov")
+})
+
+server.listen(PORT, () => {
+	console.log(`server run is port ${PORT}`)
+})
